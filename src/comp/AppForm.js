86fa9d3800dafed3d15e7463b49054dd86b2e247
Diego <diego2003coila@gmail.com>
2023-10-24 19:38:23 -0500

    v6agregaciondesubidaddedatos

--- src/comp/AppForm.js
+++ src/comp/AppForm.js
@@ -4,13 +4,35 @@ import firebase, {db} from  './firebase';
 
 const AppForm = () => {
 
+  const camposRegistro ={nombre:"", edad:"", genero:""};
+  const [objeto, setObjeto] = useState(camposRegistro);
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    try{
+      if (db){
+        addDoc(collection(db,'persona'), objeto)
+        console.log("Se guardo con exito");
+      }
+    } catch (error){
+      console.error();
+    }
+  }
   return (
     <div style={{background:"red", padding:"10px", textAlign:"center"}}>
+    <form onSubmit={handleSubmit}>
     <h1>AppFormrmr</h1>
+    <input type="text" placeholder="Nombre???"></input>
     <p>Nombre</p>
     <p>Edad</p>
     <p>Genero</p>
-    <button type="button"name="Guardar">Guardar</button>
+    <select>
+      <option value="">Seleccione su genero ¿¿¿binario???</option>
+      <option value="M">Masculino</option>
+      <option value="F">Femenino</option>
+    </select>
+    <button >Guardar/Actualizar</button>
+    </form>
 
     </div>
   )
